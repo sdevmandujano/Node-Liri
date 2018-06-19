8cd6435f3606ab4fe088cd8e2896f7b5e915987a
sdevmandujano <zmandujano01@gmail.com>
2018-06-19 17:07:14 -0500

    hw

---
+++ keys.js
@@ -0,0 +1,19 @@
+
+
+exports.twitterKeys = {
+  consumer_key: 'rGLXVdjmyyO59g1yvZskv39Ad',
+  consumer_secret: 'yZlMRubuuVSi2RPP5T67svJsbHNPJ9kJLrb0NA3IM5jDkY2TCy',
+  access_token_key: '979050268124852225-kzJPEmC5ysS6X37a5n455t1bLGXuGAY',
+  access_token_secret: 'NGyFZY58WcFxlJzewGIKv22r3AX0tQwcPT1Qxk1FDbB3g',
+  }
+  
+  exports.spotifyKeys = {
+    
+ id: '7d227f1e4d834239a736293a822e8c00',
+secret: 'cee6167fb5cb47ff921a53ce77c83d7d',
+  }
+
+
+
+
+
